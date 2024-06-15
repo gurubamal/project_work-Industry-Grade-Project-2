b2ac06795c52cc0d5ff718b0677079aeaa690684
gurubamal <99092495+gurubamal@users.noreply.github.com>
2024-06-15 17:52:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,24 +23,26 @@ mvn -v
 echo "Starting build process..."
 
 # Navigate to the directory containing the pom.xml file
-
 cd /var/lib/jenkins/workspace/project2/XYZ_Technologies
 
 # Build the application using Maven
 echo "Building the Maven project..."
 mvn clean package
 
-# Navigate back to the root workspace directory
-cd ..
+# Navigate to Jenkins home directory
+cd /var/lib/jenkins
+
+# Copy Docker credentials
+echo "Copying Docker credentials to /var/lib/jenkins/.docker/config.json"
+mkdir -p /var/lib/jenkins/.docker
+cp /home/vagrant/.docker/config.json /var/lib/jenkins/.docker/config.json
 
 # Build Docker image
 echo "Building Docker image..."
-docker build -t ${DOCKER_REPO}/${IMAGE_NAME}:${IMAGE_TAG} .
+docker build -t ${DOCKER_REPO}/${IMAGE_NAME}:${IMAGE_TAG} /var/lib/jenkins/workspace/project2/XYZ_Technologies
 
-# Copy Docker credentials
-echo "Using Docker credentials from /home/vagrant/.docker/config.json"
-mkdir -p /root/.docker
-cp /home/vagrant/.docker/config.json /root/.docker/config.json
+# Use Docker configuration
+export DOCKER_CONFIG=/var/lib/jenkins/.docker
 
 # Push Docker image to the repository
 echo "Pushing Docker image to repository..."
